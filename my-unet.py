b1101041868fa5cdbc05a441c0d27d1e864eb783
Stephen <stephen.stengel@cwu.edu>
2022-02-10 06:43:43 -0800

    Success in importing scaled down images.

--- my-unet.py
+++ my-unet.py
@@ -12,13 +12,48 @@
 #using these as reference for some parts:
 #https://github.com/dexterfichuk/U-Net-Keras-Implementation
 
+print("Running imports...")
 import os
+import numpy as np
+import random
+
+from skimage.io import imread, imshow
+from skimage.transform import resize
+from matplotlib import pyplot as plt
+from skimage.util import img_as_uint
+from skimage.util import img_as_bool
+# ~ from skimage.filters import rank
+# ~ from skimage.filters.rank import otsu
+# ~ from skimage.morphology import disk
+
+GLOBAL_HACK_height, GLOBAL_HACK_width = 128, 128
+
+print("Done!")
 
 def main(args):
 	print("Hi!")
-	HACK_width, HACK_height = 128, 128
 	
-	createTrainAndTestSets()
+	print("Creating train and test sets...")
+	trainImages, trainTruth, testImages, testTruths = createTrainAndTestSets()
+	print("Done!")
+	
+	print("Showing Training stuff...")
+	randomBoy = random.randint(0, len(trainImages) - 1)
+	print("image " + str(randomBoy) + "...")
+	imshow(trainImages[randomBoy] / 255)
+	plt.show()
+	print("truth " + str(randomBoy) + "...")
+	imshow(np.squeeze(trainTruth[randomBoy]))
+	plt.show()
+	
+	print("Showing Testing stuff...")
+	randomBoy = random.randint(0, len(testImages) - 1)
+	print("image " + str(randomBoy) + "...")
+	imshow(testImages[randomBoy] / 255)
+	plt.show()
+	print("truth " + str(randomBoy) + "...")
+	imshow(np.squeeze(testTruths[randomBoy]))
+	plt.show()
 	
 	return 0
 	
@@ -28,15 +63,55 @@ def main(args):
 def createTrainAndTestSets():
 	trainImageFileNames, trainTruthFileNames, \
 		testImageFileNames, testTruthFileNames = getFileNames()
+
+	trainImages, trainTruth = getImageAndTruth(trainImageFileNames, trainTruthFileNames)
+	testImage, testTruth = getImageAndTruth(testImageFileNames, testTruthFileNames)
 	
+	return trainImages, trainTruth, testImage, testTruth
+
+		
+def getImageAndTruth(trainImageFileNames, trainTruthFileNames):
 	trainImages, trainTruth, testImage, testTruth = [], [], [], []
 	
+	for i in range(len(trainImageFileNames)):
+		print("Importing " + trainImageFileNames[i] + "...")
+		image = imread(trainImageFileNames[i])[:, :, :3] #this is pretty arcane. research later
+		image = resize( \
+				image, \
+				(GLOBAL_HACK_height, GLOBAL_HACK_width), \
+				mode="constant", \
+				preserve_range=True)
+		trainImages.append(image)
+	
+		#This part isn't quite right. I need a ompletely binary image out.
+		#I need thresholding apparently: binarized_brains = (brains > threshold_value).astype(int)
+		#https://stackoverflow.com/questions/49210078/binarize-image-data
+		truth = np.zeros((GLOBAL_HACK_height, GLOBAL_HACK_width, 1), dtype=np.bool) #need to OR the pixels that are activated.
+		truthOR = imread(trainTruthFileNames[i])
+		truthOR = resize(truthOR, (GLOBAL_HACK_height, GLOBAL_HACK_width), mode="constant")
+		# ~ truthOR = resize(truthOR, (GLOBAL_HACK_height, GLOBAL_HACK_width), mode="constant", anti_aliasing=True)
+		# ~ truthOR = resize(truthOR, (GLOBAL_HACK_height, GLOBAL_HACK_width), mode="constant", anti_aliasing=False)
+		# ~ truthOR = resize(truthOR, (GLOBAL_HACK_height, GLOBAL_HACK_width), anti_aliasing=True)
+		# ~ truthOR = resize(truthOR, (GLOBAL_HACK_height, GLOBAL_HACK_width), anti_aliasing=False)
+		# ~ truthOR = resize(truthOR, (GLOBAL_HACK_height, GLOBAL_HACK_width))
+		truthOR = np.maximum(truth, truthOR)
+		
+		truthOR = img_as_bool(truthOR)
+		truthOR = img_as_uint(truthOR)
+		
+		# ~ truthOR = np.logical_or(truth, truthOR) #Makes it to the wrong type
+		
+		trainTruth.append( truthOR )
+		
+	trainImages, trainTruth = np.asarray(trainImages), np.asarray(trainTruth)
+	
+	return trainImages, trainTruth
 
 #returns the filenames of the images for (trainImage, trainTruth),(testimage, testTruth)
 #hardcoded!
 def getFileNames():
-	trainImagePath = "../DIBCO/2017/Dataset"
-	trainTruthPath = "../DIBCO/2017/GT"
+	trainImagePath = "../DIBCO/2017/Dataset/"
+	trainTruthPath = "../DIBCO/2017/GT/"
 
 	trainImageFileNames, trainTruthFileNames = \
 			createTrainImageAndTrainTruthFileNames(trainImagePath, trainTruthPath)
@@ -45,8 +120,8 @@ def getFileNames():
 	
 	
 	#test image section
-	testImagePath = "../DIBCO/2016/DIPCO2016_dataset"
-	testTruthPath = "../DIBCO/2016/DIPCO2016_Dataset_GT"
+	testImagePath = "../DIBCO/2016/DIPCO2016_dataset/"
+	testTruthPath = "../DIBCO/2016/DIPCO2016_Dataset_GT/"
 	
 	testImageFileNames, testTruthFileNames = \
 			createTrainImageAndTrainTruthFileNames(testImagePath, testTruthPath)
