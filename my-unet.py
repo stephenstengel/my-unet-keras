c33ab62fac75673a68075736cdb3519847e6e1fb
Stephen <stephen.stengel@cwu.edu>
2022-03-10 23:51:02 -0800

    Added the AutoInit thingy. Tested to see it work. Must do tests to see what effect it has.

--- my-unet.py
+++ my-unet.py
@@ -35,6 +35,7 @@ from tensorflow.keras.optimizers import Adam
 from keras import Model, callbacks
 from keras import backend
 
+from autoinit import AutoInit
 
 np.random.seed(55555)
 random.seed(55555)
@@ -373,8 +374,11 @@ def createStandardUnet():
 	output = decode(conv5, conv4, conv3, conv2, conv1)
 	model = Model(inputs, output)
 	
+	model = AutoInit().initialize_model(model)
+	
 	# ~ model.compile(optimizer = Adam(learning_rate=1e-4), loss='categorical_crossentropy',  metrics=["acc"])
 	model.compile(optimizer = "adam", loss = "binary_crossentropy",  metrics = ["acc"])
+	
 	
 	return model
 
