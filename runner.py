6e0e0a5ba45ddfd863cbc6fe6844aaa8e16a23f7
Stephen <stephen.stengel@cwu.edu>
2023-02-22 02:29:20 -0800

    Added a runner script so I don't need to type as much.

---
+++ runner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Just a little script to run all the possible combinations.
+
+import os
+
+#run each of my specifications
+sizes = [64, 128, 256, 512]
+for thisSize in sizes:
+  for batchSize in range(1, 17):
+    commandStr = "python3 my-unet.py 50000 " + str(thisSize) + " 100 " + str(batchSize)
+    # ~ print(commandStr)
+    os.system(commandStr)
